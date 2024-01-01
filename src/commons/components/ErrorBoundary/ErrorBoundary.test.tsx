89c017dcb3f7a56dc62b986da69b3c49d664f6a5
chrismeeky <47578865+chrismeeky@users.noreply.github.com>
2024-01-01 17:35:00 +0100

    removes error boundary wrapper from test

--- src/commons/components/ErrorBoundary/ErrorBoundary.test.tsx
+++ src/commons/components/ErrorBoundary/ErrorBoundary.test.tsx
@@ -1,16 +1,15 @@
-import ErrorBoundary from '.'
 import { customRender, screen } from '../../../test-utils'
 import ProblematicComponent from './ProblematicComponent.mock'
 
 describe('Error boundary', () => {
   test('should render the error boundary message', () => {
     console.error = jest.fn()
-    customRender(<ProblematicComponent />, { wrapper: ErrorBoundary })
+    customRender(<ProblematicComponent />)
     const errorTitle = screen.getByText('Whoops, something went wrong')
     expect(errorTitle).toBeInTheDocument()
   })
   test('should have a reload button', () => {
-    customRender(<ProblematicComponent />, { wrapper: ErrorBoundary })
+    customRender(<ProblematicComponent />)
     const reloadButton = screen.getByRole('button', { name: 'Try again' })
     expect(reloadButton).toBeInTheDocument()
   })
