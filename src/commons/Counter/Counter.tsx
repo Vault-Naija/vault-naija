de85f228aeed9d07381c4a2821e531c8085e72e3
Ikechukwu-attah <attahikechukwu@gmail.com>
2024-01-01 16:46:16 -0500

    setup log-rocket

--- src/commons/Counter/Counter.tsx
+++ src/commons/Counter/Counter.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 
 const Counter = () => {
   const [count, setCount] = useState<number>(0)
+  const data: any = null
   return (
     <div>
       <p>{count}</p>
+      {count === 10 && <p>{data.name}</p>}
       <button onClick={() => setCount((count) => count + 1)}>Click</button>
     </div>
   )
