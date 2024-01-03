979b18c94b05bf9f1254aad17a76e4afed67e983
chrismeeky <47578865+chrismeeky@users.noreply.github.com>
2024-01-03 17:40:55 +0100

    adds devServer configuration

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,3 +1,10 @@
+const historyApiFallback = require('connect-history-api-fallback')
+
 module.exports = {
   mode: 'production',
+  devServer: {
+    before(app) {
+      app.use(historyApiFallback())
+    },
+  },
 }
