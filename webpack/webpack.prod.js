bd13e8f26d6aa95e7cd818d6819ab33930862f8f
chrismeeky <47578865+chrismeeky@users.noreply.github.com>
2024-01-03 23:17:35 +0100

    removes public path

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,10 +1,3 @@
-const historyApiFallback = require('connect-history-api-fallback')
-
 module.exports = {
   mode: 'production',
-  devServer: {
-    before(app) {
-      app.use(historyApiFallback())
-    },
-  },
 }
